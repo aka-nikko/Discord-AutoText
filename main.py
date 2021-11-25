15a7f5e403ff54a9ed833429cde2270028799f31
Anshul Verma <54620652+AnshulVerma01@users.noreply.github.com>
2021-11-26 00:12:54 +0530

    update

--- main.py
+++ main.py
@@ -32,37 +32,40 @@ def msg(variable):
     send_message(get_connection(), config.channel_ID,
                  dumps(message_data))
 
+
 if __name__ == '__main__':
+    if (config.auth_token == "" or config.channel_ID=="" or config.channel_URL==""):
+        print("Enter configurations in the \"config.py\" file.")
+        exit()
     try:
         msg_list = []
         time_list = []
         count = 0
-        num = int(input("Number Many Messages To Send : "))
-        for i in range(0,num):
-            message = input("Enter Message "+str(i+1)+" : ")
-            time = int(input("Time (In Seconds) To Wait After Message "+str(i+1)+" : "))
+        num = int(input("Number of messages to send : "))
+        for i in range(0, num):
+            message = input("Enter message "+str(i+1)+" : ")
+            time = int(
+                input("Time (in seconds) to wait after message "+str(i+1)+" : "))
             msg_list.append(message)
             time_list.append(time)
-        loop = int(input("Number Of Times To Send Message (0 for infinite) : "))
-        if (loop==0):
+        loop = int(input("Number of times to send message (0 for infinite) : "))
+        if (loop == 0):
             while True:
                 count = count+1
                 print("Loop Number "+str(count))
-                for j in range(0,num):
+                for j in range(0, num):
                     print("Sending Message "+str(j+1))
                     msg(msg_list[j])
                     sleep(time_list[j])
-        elif(loop>0):
+        elif(loop > 0):
             k = 1
-            while k<=loop:
+            while k <= loop:
                 count = count+1
                 print("Loop Number "+str(count))
-                for j in range(0,num):
+                for j in range(0, num):
                     print("Sending Message "+str(j+1))
                     msg(msg_list[j])
                     sleep(time_list[j])
-                k=k+1
+                k = k+1
     except ValueError:
-        print("Enter a Valid Integer!!")
-        
-    
+        print("Enter a valid integer!!")
